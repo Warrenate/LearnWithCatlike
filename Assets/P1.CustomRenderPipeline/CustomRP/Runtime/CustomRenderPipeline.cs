a4d5989c563cae060b0b6d73a4670f3f5d40ff22
warrenate <jtlxg@sina.com>
2020-06-01 12:02:30 +0800

    add scene.

---
+++ Assets/P1.CustomRenderPipeline/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        throw new System.NotImplementedException();
+    }
+}
